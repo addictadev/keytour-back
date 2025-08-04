085498fa1d22167ded8eac88588cf2efb4383dee
M-Metaw3 <mahmoudmetawea161@gmail.com>
2025-08-04 14:56:43 +0200

    Refactor CORS configuration in index.js
    - Simplified CORS middleware setup by allowing all origins with a wildcard.
    - Removed specific method and header configurations to streamline the implementation.
    - This change aims to enhance code readability while maintaining basic CORS functionality.

--- index.js
+++ index.js
@@ -56,12 +56,7 @@ require("./src/app");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors('*'));
 // MongoDB connection
 // mongoose.connect(process.env.DB, {
 //     useNewUrlParser: true,
