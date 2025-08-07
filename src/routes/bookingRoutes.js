ec32bcd5c05d6cd057a75a663b9ccad299e269e7
M-Metaw3 <mahmoudmetawea161@gmail.com>
2025-08-07 14:09:01 +0200

    Refactor authentication middleware usage in bookingRoutes
    - Commented out the previous authentication middleware setup for clarity.
    - Integrated the AuthMiddleware.requireAuth function directly into the GET and POST routes for better security enforcement.
    - Aims to enhance code readability and maintainability by ensuring consistent authentication checks across booking operations.

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -5,14 +5,18 @@ const AuthMiddleware = require('../../V2/middleware/authMiddleware');
 const PermissionMiddleware = require('../../V2/middleware/permissionMiddleware');
 
 const router = express.Router();
-router.use(AuthMiddleware.requireAuth({
-    requireEmailVerification: false,
-    validateSession: true,
-    allowedUserTypes: ['staff']
-}));
+// router.use(AuthMiddleware.requireAuth({
+//     requireEmailVerification: false,
+//     validateSession: true,
+//     allowedUserTypes: ['staff']
+// }));
 router.post('/cancel-booking', auth(), BookingController.cancelBookingController);
 router
-    .route('/').get(auth('vendor',"admin"),PermissionMiddleware.attachPermissionInfo, PermissionMiddleware.requirePermissions('read:permissions', 'read:roles'),BookingController.getallbooking)
+    .route('/').get(auth('vendor',"admin"),AuthMiddleware.requireAuth({
+        requireEmailVerification: false,
+        validateSession: true,
+        allowedUserTypes: ['staff']
+    }),PermissionMiddleware.attachPermissionInfo, PermissionMiddleware.requirePermissions('read:permissions', 'read:roles'),BookingController.getallbooking)
     .post(auth('user',"vendor"), BookingController.createBooking).delete(BookingController.deletemanyBooking);
 
 router
@@ -21,7 +25,11 @@ router
 
 router
     .route('/:id/cancel')
-    .post(auth('vendor', 'admin'),PermissionMiddleware.attachPermissionInfo, PermissionMiddleware.requirePermissions('read:permissions', 'read:roles'), BookingController.cancelBooking);
+    .post(auth('vendor', 'admin'),AuthMiddleware.requireAuth({
+        requireEmailVerification: false,
+        validateSession: true,
+        allowedUserTypes: ['staff']
+    }),PermissionMiddleware.attachPermissionInfo, PermissionMiddleware.requirePermissions('read:permissions', 'read:roles'), BookingController.cancelBooking);
     router
     .route('/:id')
     .get(BookingController.getBookingById)
